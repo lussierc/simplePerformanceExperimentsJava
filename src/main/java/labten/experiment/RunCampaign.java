a5365e35e105b91cfe5a6306083b330f3f5fdb50
Devin Ho <hod@aldenv139.allegheny.edu>
2018-04-20 14:17:25 -0400

    Experiment folder - updated with source code.

--- src/main/java/labten/experiment/RunCampaign.java
+++ src/main/java/labten/experiment/RunCampaign.java
@@ -1,1 +1,46 @@
+package labten.experiment;
 
+import labten.computation.ArithmeticComputation;
+import labten.data.ResultsTable;
+
+import java.util.Random;
+
+/**
+ * A RunCampaign class that can run an experiment with a computer.
+ *
+ * @author Gregory M. Kapfhammer
+ */
+
+public class RunCampaign implements Campaign {
+
+  /** The starting size for running an experiment campaign. */
+  private static final int INPUT_SIZE_START = 1;
+
+  /** The growth factor for input when running an experiment campaign. */
+  private static final int INPUT_GROWTH_FACTOR = 2;
+
+  /** The indentation level for the output. */
+  private static final String IDENTATION = "  ";
+
+  /** Run a provided ArithmeticComputation in an experiment campaign for specified rounds. */
+  public ResultsTable run(ArithmeticComputation compute, int campaignLength) {
+    int campaignRound = 0;
+    int currentInputSize = INPUT_SIZE_START;
+    ResultsTable results = new ResultsTable(campaignLength);
+    System.out.println("Starting a campaign of experiments with " + compute.getName() + " ...");
+    while (campaignRound < campaignLength) {
+      long timeBefore = System.currentTimeMillis();
+      compute.compute(currentInputSize);
+      long timeAfter = System.currentTimeMillis();
+      long timeElapsed = timeAfter - timeBefore;
+      results.addResult((long)currentInputSize, timeElapsed);
+      System.out.println(IDENTATION + "Running round " + campaignRound
+          + " with input size " + currentInputSize);
+      currentInputSize = currentInputSize * INPUT_GROWTH_FACTOR;
+      campaignRound++;
+    }
+    System.out.println("... Finishing a campaign of experiments with " + compute.getName());
+    return results;
+  }
+
+}
