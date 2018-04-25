7e04b930925b92ec4f951e7c2e4c9823e37ce8f6
Lancaster Wu <31478979+Lancasterwu@users.noreply.github.com>
2018-04-25 13:42:24 -0400

    Create RecursiveFibonacciHashMap.java

---
+++ src/main/java/labten/computation/RecursiveFibonacciHashMap.java
@@ -0,0 +1,45 @@
+package labten.computation;
+
+import java.util.HashMap;
+
+
+public class RecursiveFibonacciComputationHashMap extends FibonacciComputation {
+  private HashMap<Integer, Integer> map;
+
+  /** Configure the name of the arithmetic computation. */
+  public RecursiveFibonacciComputationHashMap() {
+    // Give the name of this computation
+    name = "RecursiveFibonacciComputationHashMap";
+    map = new HashMap<>();
+  }
+
+  /**
+   * Recursively calculate the valueth Fibonacci number using longs.
+   *
+   * @param value indicates which Fibonacci number to compute.
+   * @return the valueth Fibonacci number.
+   */
+  public long compute(int value) {
+    // Zero Case:
+    // If value == 0 then fib(value) = 0.
+    if (value == 0) {
+      // return a value
+      return value;
+    } else if (value < 2) {
+      // Base Case:
+      // If value <= 2 then fib(value) = 1.
+      // return a value
+      return 1;
+    } else if (map.containsKey(value)) {
+      return map.get(value);
+    } else {
+      // Recursive Case:
+      // Otherwise, return fib(value-1) + fib(value-2)
+      // recursively call the method and return value
+      long fibonacciValue = compute(value - 2) + compute(value - 1);
+      map.put(value, (int)fibonacciValue);
+      return fibonacciValue;
+    }
+  }
+
+}
