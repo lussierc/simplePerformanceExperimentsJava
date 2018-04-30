1605638d6b7a8ee8d19148de35132bac778970a8
Lancaster Wu <31478979+Lancasterwu@users.noreply.github.com>
2018-04-30 13:33:31 -0400

    Create List.java

---
+++ src/main/java/labten/list/List.java
@@ -0,0 +1,23 @@
+import java.util.Iterator;
+
+
+public interface List<E> extends Iterable<E> {
+
+
+  int getSize();
+
+ 
+  boolean isEmpty();
+
+
+  E get(int index) throws IndexOutOfBoundsException;
+
+  E set(int index, E element) throws IndexOutOfBoundsException;
+
+  void add(int index, E element) throws IndexOutOfBoundsException;
+
+  E remove(int index) throws IndexOutOfBoundsException;
+
+  Iterator<E> iterator();
+
+}
